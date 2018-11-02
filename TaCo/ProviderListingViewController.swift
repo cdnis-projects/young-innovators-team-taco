2bf9c99e3ad8122919e30e31e7e43e873d660303
Saeed Rahman <saeedrahman@cdnis.edu.hk>
2018-11-02 15:50:56 +0800

    Configured Provider Listing View Controller - Added UIITableView - Configured test data source

--- TaCo/ProviderListingViewController.swift
+++ TaCo/ProviderListingViewController.swift
@@ -8,8 +8,18 @@
 
 import UIKit
 
-class ProviderListingViewController: UIViewController {
-
+class ProviderListingViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel!.text = "Test"
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
