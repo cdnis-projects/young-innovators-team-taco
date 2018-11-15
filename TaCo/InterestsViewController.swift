34c9cd543b56b74977b0e15d21520842c42ab0fb
Thomas Ng <thomasng2023@cdnis.edu.hk>
2018-11-15 21:55:58 +0800

    Asset Commit: - Created array of image assets for different interests in Interests View Controller

--- TaCo/InterestsViewController.swift
+++ TaCo/InterestsViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 var subjects = ["Math", "Science", "Mandarin", "Xcode", "C+", "Poster Design", "Logo Design", "Aerial Photography", "Child Care", "3D Printing", "Web Design"]
 
+
+var subjectImages: [UIImage] = [
+    UIImage(named: "Math-Asset")!,
+    UIImage(named: "Science-Asset")!,
+    UIImage(named: "Chinese-Asset")!,
+    UIImage(named: "Xcode-Asset")!,
+    UIImage(named: "C+-Asset")!,
+    UIImage(named: "Poster-Asset")!,
+    UIImage(named: "LogoDesign-Asset")!,
+    UIImage(named: "Aerial-Asset")!,
+    UIImage(named: "Childcare-Asset")!,
+    UIImage(named: "3dprinting-Asset")!,
+    UIImage(named: "Webdesign-Asset")!
+    
+]
+
 var row = 0
 
 class InterestsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return subjects.count
     }
@@ -21,6 +38,7 @@ class InterestsViewController: UIViewController, UITableViewDelegate, UITableVie
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         
         cell.textLabel?.text = subjects[indexPath.row]
+        cell.imageView?.image = subjectImages[indexPath.row]
         
         return cell 
     }
@@ -33,6 +51,8 @@ class InterestsViewController: UIViewController, UITableViewDelegate, UITableVie
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Number of Subjects: ", subjects.count)
+        print(subjects)
     }
 
 
